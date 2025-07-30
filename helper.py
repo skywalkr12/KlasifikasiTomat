5b40b64759b6b7550df693b4a17041b625312f96
skywalkr12 <syahrulfarhan05@gmail.com>
2025-07-30 09:48:36 +0700

    LFS

--- helper.py
+++ helper.py
@@ -79,7 +79,7 @@ class ResNet18(nn.Module):
             )
         )
         self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
+            nn.AdaptiveAvgPool2d(4),
             nn.Flatten(),
             nn.Dropout(0.3),
             nn.Linear(512, num_diseases)
