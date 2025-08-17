27b0c624b98c865ae43d724c0e6727ab6760412b
skywalkr12 <135092758+skywalkr12@users.noreply.github.com>
2025-08-17 23:12:19 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,6 +5,20 @@ import torchvision.transforms as transforms
 import streamlit as st
 
 # --------- Base Class ---------
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
+
 class ImageClassificationBase(nn.Module):
     def training_step(self, batch):
         images, labels = batch 
