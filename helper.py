3d2247fb4bb741f6db2aee74db990db6004c9ebb
skywalkr12 <135092758+skywalkr12@users.noreply.github.com>
2025-08-20 02:29:09 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -99,7 +99,7 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomVerticalFlip(p=0.5),
     transforms.RandomRotation(degrees=30),
-    transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
     transforms.ToTensor()
     # Jika training pakai Normalize, aktifkan lagi di sini:
     # transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
@@ -352,4 +352,5 @@ def show_prediction_and_cam(
     )
 
     return overlay, cam, used_idx, probs_raw
+
 
