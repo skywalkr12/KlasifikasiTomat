3fa4ea5796b55907e3f742ad6e8f72f3673cc41c
skywalkr12 <135092758+skywalkr12@users.noreply.github.com>
2025-08-27 20:55:46 +0700

    Update Beranda.py

--- Beranda.py
+++ Beranda.py
@@ -19,7 +19,7 @@ st.image("Beranda_tomat.jpeg", use_container_width=True)
 st.header("🌱 Tentang Aplikasi Ini")
 st.markdown("""
 Aplikasi ini dikembangkan untuk membantu petani dan penggemar tanaman dalam mengidentifikasi penyakit pada daun tomat menggunakan Model Convolutional Neural Network (CNN) berbasis **ResNet**. 
-Tujuan kami adalah menyediakan alat diagnostik awal yang cepat dan mudah digunakan.
+Tujuan dibuatnya website ini adalah menyediakan visualisasi hasil klasifikasi Penyakit Tanaman Tomat secara informatif
 """)
 st.markdown("[Pelajari Lebih Lanjut tentang ResNet](https://pytorch.org/hub/pytorch_vision_resnet/)") # Ganti dengan link relevan
 st.markdown("---")
@@ -39,6 +39,7 @@ st.markdown("""
 <a href="https://www.facebook.com/skywalkr12" target="blank_">Facebook</a>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
