e966ad1a8f9ceb702934fb31ccf53169f30b38a0
skywalkr12 <135092758+skywalkr12@users.noreply.github.com>
2025-08-27 09:34:06 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -96,7 +96,6 @@ CLASS_NAMES = [
 # ========= Transform (samakan dengan training!) =========
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor()
 ])
 
@@ -380,5 +379,6 @@ def show_prediction_and_cam(
     )
 
     return overlay, cam, used_idx, probs_raw
+
 
 
